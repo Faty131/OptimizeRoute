2d1e5d572259b2436eb29dc732c6a41a943947cf
bachri fatima ezzahra <109394388+Faty131@users.noreply.github.com>
2024-09-14 13:00:03 +0100

    Initial commit

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,15 @@
+# routes.py
 from flask import Blueprint, request, jsonify
-from .utils import generate_map_url, optimize_route
+from .utils import RouteOptimizer, MapURLGenerator
 
 main = Blueprint('main', __name__)
 
 @main.route('/generate-map-url', methods=['POST'])
 def generate_map_url_route():
-    return generate_map_url(request)
+    generator = MapURLGenerator(request.json)
+    return generator.generate()
 
 @main.route('/optimize-route', methods=['POST'])
 def optimize_route_route():
-    return optimize_route(request)
+    optimizer = RouteOptimizer(request.json)
+    return optimizer.optimize()
--- app/utils.py
+++ app/utils.py
@@ -4,111 +4,98 @@ from flask import jsonify
 from datetime import datetime
 import pytz
 
-def get_formatted_date():
-    tz = pytz.timezone('Africa/Casablanca')
-    date = datetime.now(tz)
-    formatted_date = date.strftime('%Y-%m-%dT%H:%M:%S')
-    offset = date.strftime('%z')
-    encoded_offset = '%2B' + offset[1:] if offset.startswith('+') else offset
-    return f"{formatted_date}{encoded_offset}"
+class RouteOptimizer:
+    def __init__(self, request_data):
+        self.uuid = request_data.get('uuid')
+        self.start_position = request_data.get('startPosition')
+        self.parameters = request_data.get('parameters', {})
+        self.points = request_data.get('points', [])
+        self.here_api_key = os.getenv('HERE_API_KEY')
+    
+    def get_formatted_date(self):
+        tz = pytz.timezone('Africa/Casablanca')
+        date = datetime.now(tz)
+        formatted_date = date.strftime('%Y-%m-%dT%H:%M:%S')
+        offset = date.strftime('%z')
+        encoded_offset = '%2B' + offset[1:] if offset.startswith('+') else offset
+        return f"{formatted_date}{encoded_offset}"
 
-def optimize_route(request):
-    try:
-        # Récupérer les données de la requête
-        data = request.json
-        here_api_key = os.getenv('HERE_API_KEY')
-        uuid = data.get('uuid')
-        start_position = data.get('startPosition')
-        parameters = data.get('parameters', {})
-        points = data.get('points', [])
-
-        # Déterminer les paramètres d'optimisation
-        improve_for = 'time'
-        mode = 'fastest;car;traffic:enabled'
-        clustering = ''
-
-        if parameters.get('optimizeForFuel'):
-            improve_for = 'distance'
-        elif parameters.get('optimizeForTime'):
+    def optimize(self):
+        try:
             improve_for = 'time'
+            mode = 'fastest;car;traffic:enabled'
+            clustering = ''
 
-        if parameters.get('minimizeStops'):
-            clustering = 'clustering=drivingDistance:500'
+            if self.parameters.get('optimizeForFuel'):
+                improve_for = 'distance'
+            elif self.parameters.get('optimizeForTime'):
+                improve_for = 'time'
 
-        # Construire l'URL de l'API
-        waypoints = '&'.join(
-            f'destination{index + 1}={requests.utils.quote(f"{point['designation']};{point['latitude']},{point['longitude']}")}'
-            for index, point in enumerate(points)
-        )
-        departure_time = get_formatted_date()
-        api_url = (
-            f"https://wps.hereapi.com/v8/findsequence2?start={start_position['latitude']},{start_position['longitude']}"
-            f"&{waypoints}&improveFor={improve_for}&mode={mode}&departure={departure_time}"
-            f"{'&' + clustering if clustering else ''}&apikey={here_api_key}"
-        )
+            if self.parameters.get('minimizeStops'):
+                clustering = 'clustering=drivingDistance:500'
 
-        # Appeler l'API HERE
-        response = requests.get(api_url)
-        response.raise_for_status()  # Lancer une exception pour les erreurs HTTP
-        response_data = response.json()
+            waypoints = '&'.join(
+                f'destination{index + 1}={requests.utils.quote(f"{point['designation']};{point['latitude']},{point['longitude']}")}'
+                for index, point in enumerate(self.points)
+            )
+            departure_time = self.get_formatted_date()
+            api_url = (
+                f"https://wps.hereapi.com/v8/findsequence2?start={self.start_position['latitude']},{self.start_position['longitude']}"
+                f"&{waypoints}&improveFor={improve_for}&mode={mode}&departure={departure_time}"
+                f"{'&' + clustering if clustering else ''}&apikey={self.here_api_key}"
+            )
 
-        # Journaliser la réponse de l'API
-        print(f"API Response: {response_data}")
+            response = requests.get(api_url)
+            response.raise_for_status()  
+            response_data = response.json()
 
-        # Traiter les points optimisés
-        optimized_waypoints = response_data['results'][0]['waypoints']
-        optimized_points = [
-            {**point, 'order': index + 1}
-            for index, wp in enumerate(optimized_waypoints[1:])
-            for point in points if point['designation'] == wp['id']
-        ]
+            optimized_waypoints = response_data['results'][0]['waypoints']
+            optimized_points = [
+                {**point, 'order': index + 1}
+                for index, wp in enumerate(optimized_waypoints[1:])
+                for point in self.points if point['designation'] == wp['id']
+            ]
 
-        # Convertir les valeurs de distance et durée
-        distance = int(response_data['results'][0]['distance'])
-        duration = int(response_data['results'][0]['time'])
+            distance = int(response_data['results'][0]['distance'])
+            duration = int(response_data['results'][0]['time'])
 
-        result = {
-            'uuid': uuid,
-            'distance': distance / 1000,  # Convertir mètres en kilomètres
-            'duration': duration / 60,    # Convertir secondes en minutes
-            'startPosition': start_position,
-            'parameters': parameters,
-            'points': optimized_points,
-        }
+            result = {
+                'uuid': self.uuid,
+                'distance': distance / 1000,  
+                'duration': duration / 60,   
+                'startPosition': self.start_position,
+                'parameters': self.parameters,
+                'points': optimized_points,
+            }
 
-        return jsonify(result)
-    except requests.exceptions.RequestException as req_err:
-        print(f'HTTP Request Error: {req_err}')
-        return jsonify({'error': 'HTTP Request Error'}), 500
-    except ValueError as val_err:
-        print(f'Value Error: {val_err}')
-        return jsonify({'error': str(val_err)}), 400
-    except Exception as e:
-        print(f'Unexpected Error: {e}')
-        return jsonify({'error': 'Error optimizing route'}), 500
+            return jsonify(result)
+        except requests.exceptions.RequestException as req_err:
+            print(f'HTTP Request Error: {req_err}')
+            return jsonify({'error': 'HTTP Request Error'}), 500
+        except ValueError as val_err:
+            print(f'Value Error: {val_err}')
+            return jsonify({'error': str(val_err)}), 400
+        except Exception as e:
+            print(f'Unexpected Error: {e}')
+            return jsonify({'error': 'Error optimizing route'}), 500
 
 
-def generate_map_url(request):
-    try:
-        data = request.json
-        uuid = data.get('uuid')
-        start_position = data.get('startPosition')
-        points = data.get('points', [])
+class MapURLGenerator:
+    def __init__(self, request_data):
+        self.uuid = request_data.get('uuid')
+        self.start_position = request_data.get('startPosition')
+        self.points = request_data.get('points', [])
 
-        if not uuid or not start_position or not points or not isinstance(points, list) or len(points) == 0:
+    def generate(self):
+        if not self.uuid or not self.start_position or not self.points or not isinstance(self.points, list) or len(self.points) == 0:
             return jsonify({'error': 'Invalid input format'}), 400
 
-        ordered_points = sorted(points, key=lambda p: p.get('order', 0))
-        origin = f"{start_position['latitude']},{start_position['longitude']}"
-        destination = f"{ordered_points[-1]['latitude']},{ordered_points[-1]['longitude']}"
-        waypoints = '|'.join(f"{p['latitude']},{p['longitude']}" for p in ordered_points[:-1])
+        ordered_points = sorted(self.points, key=lambda p: p.get('order', 0))
 
-        map_url = (
-            f"https://www.google.com/maps/dir/?api=1&origin={origin}"
-            f"&destination={destination}&waypoints={waypoints}"
-        )
+        origin = f"{self.start_position['latitude']}_{self.start_position['longitude']}"
+        waypoints = ''.join([f"~pos.{p['latitude']}_{p['longitude']}" for p in ordered_points[:-1]])
+        destination = f"~pos.{ordered_points[-1]['latitude']}_{ordered_points[-1]['longitude']}"
 
-        return jsonify({'map_url': map_url})
-    except Exception as e:
-        print(f'Error generating map URL: {e}')
-        return jsonify({'error': 'Error generating map URL'}), 500
+        map_url = f"https://www.bing.com/maps?rtp=pos.{origin}{waypoints}{destination}"
+
+        return jsonify({'mapUrl': map_url})
